4a64d2f1e56eff81941bb71a9040947fa553b8a8
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2022-06-10 11:03:39 +0700

    add new field

--- src/main/java/idx/kafka/producer/IdxKafkaProducer.java
+++ src/main/java/idx/kafka/producer/IdxKafkaProducer.java
@@ -34,8 +34,6 @@ import org.springframework.boot.ApplicationArguments;
 @Service
 public class IdxKafkaProducer {
 
-    @Value("${kafka-config}")
-    private String kafkaConfig;
 
     // Create topic in Confluent Cloud
     public static void createTopic(final String topic,
@@ -59,7 +57,8 @@ public class IdxKafkaProducer {
         // Create the configuration file (e.g. at '$HOME/.confluent/java.config') with configuration parameters
         // to connect to your Kafka cluster, which can be on your local host, Confluent Cloud, or any other cluster.
         // Follow these instructions to create this file: https://docs.confluent.io/platform/current/tutorials/examples/clients/docs/java.html
-        final Properties props = loadConfig(this.kafkaConfig);
+        System.out.println(applicationArguments.toString());
+        final Properties props = loadConfig("kafka.config");
 
         // Create topic if needed
         final String topic = topic_name;
