44cdeed703367e05a9f23fd44c688e6ea84dcd8e
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2022-06-10 10:58:45 +0700

    add new field

--- src/main/java/idx/kafka/producer/BodyResponsesController.java
+++ src/main/java/idx/kafka/producer/BodyResponsesController.java
@@ -1,6 +1,7 @@
 package idx.kafka.producer;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.io.IOException;
 
 @RestController
 @Configuration
+@Controller
 class BodyResponsesController {
 
     private final BodyResponsesInterface repository;
