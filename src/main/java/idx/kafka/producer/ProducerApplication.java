9c47143a42a0ebe5f3c9ead10ea15fb8d2e07958
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2022-06-10 11:37:27 +0700

    add new field

--- src/main/java/idx/kafka/producer/ProducerApplication.java
+++ src/main/java/idx/kafka/producer/ProducerApplication.java
@@ -12,6 +12,7 @@ public class ProducerApplication {
 		SpringApplication app = new SpringApplication(ProducerApplication.class);
 		app.setDefaultProperties(Collections
 				.singletonMap("server.port", "8083"));
+		System.out.println(args);
 		app.run(args);
 	}
 
