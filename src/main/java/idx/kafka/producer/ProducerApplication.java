655b51ec138182ac21407a6a6af5ad62518c8d3a
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2022-06-10 14:27:48 +0700

    add new field

--- src/main/java/idx/kafka/producer/ProducerApplication.java
+++ src/main/java/idx/kafka/producer/ProducerApplication.java
@@ -1,22 +1,27 @@
 package idx.kafka.producer;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
 
 @SpringBootApplication
 public class ProducerApplication {
 	public static void main(String[] args) {
+		if (args.length < 2){
 
+			System.out.println("please run : java -jar app.jar listener_port --config=kafka_config.config");
+			System.exit(0);
+		}
 		SpringApplication app = new SpringApplication(ProducerApplication.class);
 		app.setDefaultProperties(Collections
 				.singletonMap("server.port", "8083"));
-		for (String arg:args
-			 ) {
-			System.out.println(arg);
-		}
+
 		app.run(args);
 	}
-
 }
